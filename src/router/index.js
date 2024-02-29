0b4da409630161108ebea9a8769addc003e6c052
Kristian Vasilev <vasilevkrizztian@gmail.com>
2024-02-29 21:19:13 +0200

    final

--- src/router/index.js
+++ src/router/index.js
@@ -53,7 +53,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
